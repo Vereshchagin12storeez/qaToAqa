92fd4be4f07d0aa783760c521b15a9b9d89e55b7
Vereshchagin Evgeniy <e.vereshchagin@12storeez.com>
2024-02-13 09:31:56 +0300

    Add answers

--- Python/Lesson_1/task2*.py
+++ Python/Lesson_1/task2*.py
@@ -6,7 +6,7 @@
 """
 products_in_shop = {"молоко": 5, "хлеб": 3, "вода": 4, "масло": 0}
 # Для того чтобы получить value из словаря можно использовать: products_in_shop.get("молоко/другое значение")
-# Для того чтобы изменить value в словаря: products_in_shop["молоко/другой ключ"] = новое значение
+# Для того чтобы изменить value в словаре: products_in_shop["молоко/другой ключ"] = новое значение
 
 
 def shop(product, amount=1):
@@ -19,4 +19,24 @@ product_to_buy = input("Введите товар который хотите п
 amount_of_product = int(input("Введите желаемое количество товара: "))
 
 result = shop(product_to_buy)
-print(result)
+
+
+# def shop(product, amount):
+#     product_real_amount = products_in_shop.get(product)
+#     if not product_real_amount:  # в случае если такого ключа нет - вернётся значение None(False по сути)
+#         return "Товара нет в магазине"
+#
+#     if product_real_amount < amount:
+#         return f"В магазине нет столько {product}. Осталось всего {product_real_amount}"
+#
+#     products_in_shop[product] -= amount
+#     return "Товар приобретён"
+#
+#
+# for i in range(1, 6):
+#     print(f"Поход в магазин №{i}.")
+#     product_to_buy = input("Введите товар который хотите приобрести: ").lower()
+#     amount_of_product = int(input("Введите желаемое количество товара: "))
+#
+#     result = shop(product_to_buy, amount_of_product)
+#     print(result)
