2ded8d23ab3da3fcb4377f9c536cc5e72dbc1e24
eugene.vereshchagin <e.vereshchagin@12storeez.com>
2024-06-05 14:55:36 +0300

    Lesson 2

---
+++ Ksenia&Alena/1_PythonLessons/Lesson_2/main.py
@@ -0,0 +1,109 @@
+string = "qweeeErty 123"
+
+# print(string.split(" "))
+
+# print(string.find("r"))
+
+# print(string.count("e"))
+
+# print(string.capitalize()) # первую букву к верхнему регистру
+
+# print(string.endswith("3")) # проверяет что строка кончается определённым симоволом/символами
+
+# print(string.lower())
+
+# print(string[1:7:2])  # slice
+
+# print(len(string))
+
+# ____________________________________
+
+num = 23
+
+num += 1
+num = num + 1
+
+# print(num ** 2)
+
+# print(num / 2)
+
+# print(float(num))
+# print(str(num))
+
+# ____________________________________
+
+null = None
+# ____________________________________
+
+lst = [1, 2, "qwerty", [], True]
+
+
+# print(lst[2])
+
+# print(lst[::2])
+
+# lst.append("2")
+# print(lst)
+
+# print(lst.pop())
+
+# print(lst.sort())
+
+# print(lst.index(1))
+
+# lst.insert(2, "8765")
+# print(lst)
+
+# print(len(lst))
+# ____________________________________
+
+tpl = tuple(lst)
+
+# print(len(tpl))
+# print(tpl[2])
+
+# ____________________________________
+
+st = {1, 3, 5, "12345"}
+# ____________________________________
+
+dct = {"key": "value", "key1": "value1"}
+
+# dct.update({"key1": 32536})
+# print(dct)
+
+# print(dct.values())
+# print(dct.keys())
+
+# print(dct.items())
+# ____________________________________
+# a = 1
+# while a != 99999:
+#     a += 1
+#     print(a)
+# print("Finish")
+
+# while True:
+#     a += 1
+#     print(a)
+#     if a == 546:
+#         break
+# print("Finish")
+
+# ____________________________________
+
+lst = [1, 3, 4, 6]
+strng = "qwerty"
+dct1 = {"key": "value", "key1": "value1"}
+
+
+# for item in lst:
+#     print(item)
+
+# for i in range(1, 50, 2):
+#     print(i)
+
+
+# for key, value in dct1.items():
+#     print(key)
+#     print(value)
