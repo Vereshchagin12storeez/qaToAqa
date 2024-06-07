a258518d78cdc02f5924f67dab5b3117a9168f30
Alena Susloparova <susloparova.alena@gmail.com>
2024-06-07 15:26:29 +0400

    Task 1 resolved

--- Ksenia&Alena/1_PythonLessons/Lesson_2/task_1.py
+++ Ksenia&Alena/1_PythonLessons/Lesson_2/task_1.py
@@ -5,8 +5,19 @@
 
 applicant_names = ["Петя", "Света", "Вася", "Алина", "Никита"]
 success_number = 230
+places = 2
 
-russian_mark = int(input("Введите количество баллов по русскому языку: "))
-math_mark = int(input("Введите количество баллов по математике: "))
-chemistry_mark = int(input("Введите количество баллов по химии: "))
-
+for i in applicant_names:
+    if places <= 0:
+        print("Мест больше нет!")
+        break
+    print(i)
+    russian_mark = int(input("Введите количество баллов по русскому языку: "))
+    math_mark = int(input("Введите количество баллов по математике: "))
+    chemistry_mark = int(input("Введите количество баллов по химии: "))
+    summ = russian_mark + math_mark + chemistry_mark
+    if summ >= success_number:
+        print(f"{i} поступил")
+        places -= 1
+    else:
+        print(f"{i} не поступил")
