a8ee893c93952aeef41771a0163db56d88fda3d3
Alena Susloparova <susloparova.alena@gmail.com>
2024-06-19 15:08:12 +0400

    Task Alena, Fix 1

--- Ksenia&Alena/1_PythonLessons/Lesson_2/task_alena.py
+++ Ksenia&Alena/1_PythonLessons/Lesson_2/task_alena.py
@@ -3,15 +3,24 @@
 # Если редких слов несколько, нужно вывести то, которое меньше в лексикографическом порядке.
 # Регистр слов не учитывается.
 
-strng = input("Введите строку: ")
-lst = strng.split(" ")
-print(lst)
+# bus ron fen bus fen ron bus
+lst = input("Введите строку: ").split(" ")
 
 dct = {}
 for word in lst:
     if dct.get(word) is None:
         dct[word] = 1
     else:
-        dct[word] = dct.get(word) + 1
-print(dct)
+        dct[word] += 1
 
+
+min_quantity = dct[word]
+min_word = word
+for word, quantity in dct.items():
+    if dct[word] < min_quantity:
+        min_quantity = dct[word]
+        min_word = word
+    elif dct[word] == min_quantity and word < min_word:
+        min_word = word
+
+print(f"{min_word}: {min_quantity}")
