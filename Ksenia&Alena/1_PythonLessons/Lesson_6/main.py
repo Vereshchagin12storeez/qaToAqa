21ddf00e80a77482b3ae5d268219b1a65258d2f3
eugene.vereshchagin <e.vereshchagin@12storeez.com>
2024-06-28 14:50:19 +0300

    Add lesson 6.

---
+++ Ksenia&Alena/1_PythonLessons/Lesson_6/main.py
@@ -0,0 +1,32 @@
+import math as m
+from math import sqrt
+import time
+
+# number = 256
+# number_sq = m.sqrt(number)
+# number_sq = sqrt(number)
+# print(number_sq)
+
+
+print("До таймера")
+time.sleep(5)
+print("После таймера")
+
+
+# with open("file.txt", 'a') as file:
+#     file.write("qwerty")
+#     print("Канал связи открыт")
+# print("Канал связи закрыт")
+#
+# with open("file.txt", 'r') as file:
+#     text = file.read()
+#     print(text)
+#     print("Канал связи открыт")
+# print("Канал связи закрыт")
+
+
+# python3 -m venv venv - создание виртуального окружения
+# venv\Scripts\activate или source venv/bin/activate(mac, linux) - активация venv
+# pip install -r requirements.txt - установка библиотек и пакетов
+
+
